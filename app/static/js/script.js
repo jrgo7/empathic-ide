a99c284cb0c6711b4158abec00564bca2015836f
cgkghj <goaaron09@gmail.com>
2025-11-15 15:10:51 +0800

    remove broken compile button

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,168 +1,174 @@
-require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs' } })
+require.config({
+  paths: {
+    vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs",
+  },
+});
 
-
-require(['vs/editor/editor.main'], function () {
-    const editorContainer = document.getElementById('editor')
-    window.editor = monaco.editor.create(editorContainer, {
-        value: '',
-        language: 'c',
-        fontSize: 16,
-        fontFamily: "JetBrains Mono",
-        roundedSelection: true,
-        automaticLayout: true,
-        fixedOverflowWidgets: true,
-        minimap: {
-            enabled: false,
-        },
-        "bracketPairColorization.enabled": true
-    })
-    defineCatppuccinFrappeTheme()
-    defineCatppuccinLatteTheme()
-    monaco.editor.setTheme('catppuccin-latte')
-})
+require(["vs/editor/editor.main"], function () {
+  const editorContainer = document.getElementById("editor");
+  window.editor = monaco.editor.create(editorContainer, {
+    value: "",
+    language: "c",
+    fontSize: 16,
+    fontFamily: "JetBrains Mono",
+    roundedSelection: true,
+    automaticLayout: true,
+    fixedOverflowWidgets: true,
+    minimap: {
+      enabled: false,
+    },
+    "bracketPairColorization.enabled": true,
+  });
+  defineCatppuccinFrappeTheme();
+  defineCatppuccinLatteTheme();
+  monaco.editor.setTheme("catppuccin-latte");
+});
 
 async function addMessage(author, messageText) {
-    const chatMessageTemplate = document.getElementById("template-chat-message");
-    const chatHistoryDiv = document.getElementById("chat-container");
-    const chatMessageDiv = chatMessageTemplate.content.cloneNode(true);
-    chatMessageDiv.querySelector(".chat-message-author").textContent = author;
-    chatMessageDiv.querySelector(".chat-message-content").textContent = messageText;
-    // const spacerDiv = chatHistoryDiv.querySelector('.spacer');
-    chatHistoryDiv.appendChild(chatMessageDiv);
+  const chatMessageTemplate = document.getElementById("template-chat-message");
+  const chatHistoryDiv = document.getElementById("chat-container");
+  const chatMessageDiv = chatMessageTemplate.content.cloneNode(true);
+  chatMessageDiv.querySelector(".chat-message-author").textContent = author;
+  chatMessageDiv.querySelector(".chat-message-content").textContent =
+    messageText;
+  // const spacerDiv = chatHistoryDiv.querySelector('.spacer');
+  chatHistoryDiv.appendChild(chatMessageDiv);
 }
-
 
 async function addToTerminal(content) {
-    const terminal = document.getElementById("terminal-content");
-    if (Array.isArray(content)) {
-        content.forEach(line => {
-            terminal.textContent += line + "\n";
-        });
-    } else if (typeof content === 'string') {
-        terminal.textContent += content + "\n";
-    }
-    terminal.scrollTop = terminal.scrollHeight
+  const terminal = document.getElementById("terminal-content");
+  if (Array.isArray(content)) {
+    content.forEach((line) => {
+      terminal.textContent += line + "\n";
+    });
+  } else if (typeof content === "string") {
+    terminal.textContent += content + "\n";
+  }
+  terminal.scrollTop = terminal.scrollHeight;
 }
 
-document.querySelector('button#send').addEventListener("click", async function () {
-    const editorText = window.editor.getValue()
-    const textarea = document.querySelector('textarea#message-textarea')
-    const messageText = textarea.value
-    textarea.value = ''
+document
+  .querySelector("button#send")
+  .addEventListener("click", async function () {
+    const editorText = window.editor.getValue();
+    const textarea = document.querySelector("textarea#message-textarea");
+    const messageText = textarea.value;
+    textarea.value = "";
     const config = {
-        method: 'POST',
-        body: JSON.stringify({ message: messageText, code: editorText })
-    }
+      method: "POST",
+      body: JSON.stringify({ message: messageText, code: editorText }),
+    };
     await addMessage("user", messageText);
     const response = await fetch("/code/send", config);
     const responseJson = await response.json();
 
     if (response.status == 403) {
-        await addMessage(
-            "bot",
-            responseJson.error
-        );
-        return;
+      await addMessage("bot", responseJson.error);
+      return;
     }
 
-    const { acknowledgement, supportive_suggestion: supportiveSuggestion } = responseJson.reply;
-    console.log(responseJson)
-    console.log(acknowledgement)
-    console.log(supportiveSuggestion)
+    const { acknowledgement, supportive_suggestion: supportiveSuggestion } =
+      responseJson.reply;
+    console.log(responseJson);
+    console.log(acknowledgement);
+    console.log(supportiveSuggestion);
     await addMessage("bot", acknowledgement);
     await addMessage("bot", supportiveSuggestion);
-})
+  });
 
-const filePickerTypes = [{
-    description: 'C file',
-    accept: { 'text/plain': ['.c'] },
-}]
+const filePickerTypes = [
+  {
+    description: "C file",
+    accept: { "text/plain": [".c"] },
+  },
+];
 
-document.querySelector('button#open').addEventListener("click", async function () {
-    const [fileHandle] = await window.showOpenFilePicker({ types: filePickerTypes });
+document
+  .querySelector("button#open")
+  .addEventListener("click", async function () {
+    const [fileHandle] = await window.showOpenFilePicker({
+      types: filePickerTypes,
+    });
     const file = await fileHandle.getFile();
     const content = await file.text();
     window.editor.setValue(content);
-})
+  });
 
-
-document.querySelector('button#save').addEventListener("click", async function () {
-    const editorText = window.editor.getValue()
+document
+  .querySelector("button#save")
+  .addEventListener("click", async function () {
+    const editorText = window.editor.getValue();
     const handle = await showSaveFilePicker({ types: filePickerTypes });
     const blob = new Blob([editorText]);
     const writableStream = await handle.createWritable();
     await writableStream.write(blob);
     await writableStream.close();
+  });
 
-})
-
-
-document.querySelector('button#compile').addEventListener("click", async function () {
+document
+  .querySelector("button#run")
+  .addEventListener("click", async function () {
     // TODO: Display results
-    const editorText = window.editor.getValue()
+    const editorText = window.editor.getValue();
     const config = {
-        method: 'POST',
-        body: JSON.stringify({ editorText })
-    }
-    const response = await fetch("/code/compile", config)
-})
-
-
-document.querySelector('button#run').addEventListener("click", async function () {
-    // TODO: Display results
-    const editorText = window.editor.getValue()
-    const config = {
-        method: 'POST',
-        body: JSON.stringify({ code: editorText })
-    }
-    const response = await fetch("/code/run", config)
+      method: "POST",
+      body: JSON.stringify({ code: editorText }),
+    };
+    const response = await fetch("/code/run", config);
     const responseJson = await response.json();
 
     if (response.status == 403) {
-        await addMessage(
-            "bot",
-            responseJson.error
-        );
-        return;
+      await addMessage("bot", responseJson.error);
+      return;
     }
-    const { acknowledgement, supportive_suggestion: supportiveSuggestion } = responseJson.reply;
-    console.log(responseJson)
-    console.log(acknowledgement)
-    console.log(supportiveSuggestion)
+    const { acknowledgement, supportive_suggestion: supportiveSuggestion } =
+      responseJson.reply;
+    console.log(responseJson);
+    console.log(acknowledgement);
+    console.log(supportiveSuggestion);
     await addMessage("bot", acknowledgement);
     await addMessage("bot", supportiveSuggestion);
 
-    await addToTerminal(responseJson.output)
-})
+    await addToTerminal(responseJson.output);
+  });
 
-document.querySelector('button#set-api-key').addEventListener("click", async function () {
-    const apiKeyInput = document.getElementById('api-key-input');
+document
+  .querySelector("button#set-api-key")
+  .addEventListener("click", async function () {
+    const apiKeyInput = document.getElementById("api-key-input");
 
-    const config = { method: "POST", body: JSON.stringify({ gemini_api_key: apiKeyInput.value }) };
+    const config = {
+      method: "POST",
+      body: JSON.stringify({ gemini_api_key: apiKeyInput.value }),
+    };
     const response = await fetch("/code/api-key", config);
 
-    const modal = document.getElementById('modal-gemini-api-key');
+    const modal = document.getElementById("modal-gemini-api-key");
     modal.classList.remove("modal-visible");
-})
+  });
 
-document.querySelector('button#open-api-key-modal').addEventListener("click", async function () {
-    const apiKeyInput = document.getElementById('api-key-input');
+document
+  .querySelector("button#open-api-key-modal")
+  .addEventListener("click", async function () {
+    const apiKeyInput = document.getElementById("api-key-input");
     apiKeyInput.disabled = true;
     apiKeyInput.value = "Checking if API key exists...";
 
-    const modal = document.getElementById('modal-gemini-api-key');
+    const modal = document.getElementById("modal-gemini-api-key");
     modal.classList.add("modal-visible");
 
-    const config = { method: 'GET' }
+    const config = { method: "GET" };
     const response = await fetch("/code/api-key", config);
     const responseJson = await response.json();
     const geminiApiKey = responseJson.reply;
 
     apiKeyInput.value = geminiApiKey;
     apiKeyInput.disabled = false;
-})
+  });
 
-document.querySelector('button#close-api-key-modal').addEventListener("click", async function () {
-    modal = document.getElementById('modal-gemini-api-key');
+document
+  .querySelector("button#close-api-key-modal")
+  .addEventListener("click", async function () {
+    modal = document.getElementById("modal-gemini-api-key");
     modal.classList.remove("modal-visible");
-})
+  });
